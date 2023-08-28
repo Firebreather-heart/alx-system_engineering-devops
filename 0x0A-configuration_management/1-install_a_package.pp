029c8796858705cbaadc57ae5a3009d08be0e1ec
firebreather-heart <dtenny95@gmail.com>
2023-08-28 13:38:07 +0100

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+class flask_install {
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  exec { 'install_flask':
+    command     => '/usr/bin/pip3 install Flask==2.1.0',
+    path        => ['/usr/bin', '/usr/local/bin'],
+    creates     => '/usr/local/lib/python3.x/dist-packages/flask',  
+    require     => Package['python3-pip'],
+  }
+}
+
+include flask_install
